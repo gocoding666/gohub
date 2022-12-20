c00f9d42b5febfe1a1833cb267c925781bf84a2a
zhangbaoqiang <1099457817@qq.com>
2022-12-20 20:59:03 +0800

    用户模型

---
+++ app/models/model.go
@@ -0,0 +1,15 @@
+// Packagemodels模型通用属性和方法
+package models
+
+import "time"
+
+// BaseModel模型基类
+type BaseModel struct {
+	ID uint64 `gorm:"column:id;primaryKey;authIncrement;" json:"id,omitempty"`
+}
+
+// CommonTimestampsField时间戳
+type CommonTimestampsField struct {
+	CreatedAt time.Time `gorm:"column:created_at;index;" json:"created_at,omitempty"`
+	UpdatedAt time.Time `gorm:"column:updated_at;index;" json:"updated_at,omitempty"`
+}
