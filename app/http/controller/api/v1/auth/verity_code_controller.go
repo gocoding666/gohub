4abe64849ab0ab41fd1a26430a684fee60960765
zhangbaoqiang <1099457817@qq.com>
2023-01-03 21:59:09 +0800

    发送邮件验证码

--- app/http/controller/api/v1/auth/verity_code_controller.go
+++ app/http/controller/api/v1/auth/verity_code_controller.go
@@ -45,3 +45,23 @@ func (vc *VerifyCodeController) SendUsingPhone(c *gin.Context) {
 		response.Success(c)
 	}
 }
+
+// SendUsingEmail
+//
+//	@Description: SendUsingEmail 发送Email验证码
+//	@receiver vc
+//	@param c
+func (vc *VerifyCodeController) SendUsingEmail(c *gin.Context) {
+	//1.验证表单
+	request := requests.VerifyCodeEmailRequest{}
+	if ok := requests.Validate(c, &request, requests.VerifyCodeEmail); !ok {
+		return
+	}
+	//2.发送邮件
+	err := verifycode.NewVerifyCode().SendEmail(request.Email)
+	if err != nil {
+		response.Abort500(c, "发送Email验证码失败~")
+	} else {
+		response.Success(c)
+	}
+}
--- pkg/verifycode/verifycode.go
+++ pkg/verifycode/verifycode.go
@@ -1,10 +1,12 @@
 package verifycode
 
 import (
+	"fmt"
 	"gohub/pkg/app"
 	"gohub/pkg/config"
 	"gohub/pkg/helpers"
 	"gohub/pkg/logger"
+	"gohub/pkg/mail"
 	"gohub/pkg/redis"
 	"gohub/pkg/sms"
 	"strings"
@@ -68,4 +70,28 @@ func (vc *VerifyCode) generateVerifyCode(key string) string {
 	//将验证码及KEY（邮箱或手机号）存放到Redis中并设置过期时间
 	vc.Store.Set(key, code)
 	return code
+}
+
+// SendEmail 发送邮件验证码，调用示例：
+//
+//	verifycode.NewVerifyCode(),SendEmail(request.Email)
+func (vc VerifyCode) SendEmail(email string) error {
+	//生成验证码
+	code := vc.generateVerifyCode(email)
+	//方便本地和API自动测试
+	if !app.IsProduction() && strings.HasSuffix(email, config.GetString("verifycode.debug_email_suffix")) {
+		return nil
+	}
+	content := fmt.Sprintf("<h1>您的 Email 验证码是 %v </h>", code)
+	//发送邮件
+	mail.NewMailer().Send(mail.Email{
+		From: mail.From{
+			Address: config.GetString("mail.from.address"),
+			Name:    config.GetString("mail.from.name"),
+		},
+		To:      []string{email},
+		Subject: "Email 验证码",
+		HTML:    []byte(content),
+	})
+	return nil
 }
