ddea2564842539a53d920ae89e7bf08e8bb0419e
zhangbaoqiang <1099457817@qq.com>
2023-01-07 10:56:12 +0800

    手机+短信验证码登录

---
+++ app/http/controller/api/v1/auth/login_controller.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	v1 "gohub/app/http/controller/api/v1"
+	"gohub/app/requests"
+	"gohub/pkg/auth"
+	"gohub/pkg/jwt"
+	"gohub/pkg/response"
+)
+
+// LoginController 用户控制器
+type LoginController struct {
+	v1.BaseAPIController
+}
+
+// LoginByPhone 手机登录
+func (lc *LoginController) LoginByPhone(c *gin.Context) {
+	// 1.验证表单
+	request := requests.LoginByPhoneRequest{}
+	if ok := requests.Validate(c, &request, requests.LogingByPhone); !ok {
+		return
+	}
+	// 2.尝试登录
+	user, err := auth.LoginByPhone(request.Phone)
+	if err != nil {
+		response.Error(c, err, "账号不存在")
+	} else {
+		//登录成功
+		token := jwt.NewJWT().IssueToken(user.GetStringID(), user.Name)
+		response.JSON(c, gin.H{
+			"token": token,
+		})
+	}
+}
--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -13,3 +13,13 @@ func IsPhoneExist(phone string) bool {
 	database.DB.Model(User{}).Where("phone = ?", phone).Count(&count)
 	return count > 0
 }
+
+// GetByPhone通过手机号来获取用户
+func GetByPhone(phone string) (userModel User) {
+	database.DB.Where("phone = ? ", phone).First(&userModel)
+	return
+}
+func GetByMulti(loginID string) (userModel User) {
+	database.DB.Where("phone = ? ", loginID).Or("email = ? ", loginID).Or("name= ?", loginID).First(&userModel)
+	return
+}
