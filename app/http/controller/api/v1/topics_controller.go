dd6442b08521c262397e19a6393b85d64da141bd
zhangbaoqiang <1099457817@qq.com>
2023-01-30 21:06:20 +0800

    删除话题

--- app/http/controller/api/v1/topics_controller.go
+++ app/http/controller/api/v1/topics_controller.go
@@ -60,3 +60,23 @@ func (ctrl *TopicsController) Update(c *gin.Context) {
 		response.Abort500(c, "更新失败，请稍后尝试~")
 	}
 }
+
+func (ctrl *TopicsController) Delete(c *gin.Context) {
+	topicModel := topic.Get(c.Param("id"))
+	if topicModel.ID == 0 {
+		response.Abort404(c)
+		return
+	}
+
+	if ok := policies.CanModifyTopic(c, topicModel); !ok {
+		response.Abort403(c)
+		return
+	}
+
+	rowAffected := topicModel.Delete()
+	if rowAffected > 0 {
+		response.Success(c)
+		return
+	}
+	response.Abort500(c, "删除失败，请稍后尝试")
+}
