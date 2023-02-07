a83e3b8d1636c0715fb9cf9587a2a49d464aaafa
zhangbaoqiang <1099457817@qq.com>
2023-02-07 22:19:50 +0800

    友情链接模型和迁移new

--- database/migrations/2023_01_31_183240_add_links_table.go
+++ database/migrations/2023_01_31_183240_add_links_table.go
@@ -1,29 +1,26 @@
-package migrations 
+package migrations
 
 import (
-    "database/sql"
-    "gohub/app/models"
-    "gohub/pkg/migrate"
+	"database/sql"
+	"gohub/app/models"
+	"gohub/pkg/migrate"
 
-    "gorm.io/gorm"
+	"gorm.io/gorm"
 )
 
 func init() {
-    type User struct {
-        models.BaseModel
-        Name string `gorm:"type:varchar(255);not null;index"`
-        Email string `gorm:"type:varchar(255);index;default:null"`
-        Phone string `gorm:"type:varchar(255);index;default:null"`
-        Password string `gorm:"type:varchar(255)"`
-        models.CommonTimestampsField
-    }
+	type Link struct {
+		models.BaseModel
+		Name string `gorm:"type:varchar(255);not null"`
+		URL  string `gorm:"type:varchar(255);default:null"`
+		models.CommonTimestampsField
+	}
 
-    up := func(migrator gorm.Migrator,DB *sql.DB){
-        migrator.AutoMigrate(&User{})
-    }
-    down := func(migrator gorm.Migrator,DB *sql.DB){
-        migrator.DropTable(&User{})
-    }
-    migrate.Add("2023_01_31_183240_add_links_table",up,down)
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Link{})
+	}
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Link{})
+	}
+	migrate.Add("2023_01_31_183240_add_links_table", up, down)
 }
-
