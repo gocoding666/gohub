5d25759965ef2c9e40eddaa2c28fe3fc33f4e1a1
zhangbaoqiang <1099457817@qq.com>
2023-02-11 18:53:04 +0800

    cache clear 命令

---
+++ app/cmd/cache.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"gohub/pkg/cache"
+	"gohub/pkg/console"
+
+	"github.com/spf13/cobra"
+)
+
+var CmdCache = &cobra.Command{
+	Use:   "cache",
+	Short: "Cache management",
+}
+var CmdCacheClear = &cobra.Command{
+	Use:   "clear",
+	Short: "Clear cache",
+	Run:   runCacheClear,
+}
+
+func init() {
+	//注册cache命令的子命令
+	CmdCache.AddCommand(CmdCacheClear)
+}
+
+func runCacheClear(cmd *cobra.Command, args []string) {
+	cache.Flush()
+	console.Success("Cache cleared.")
+}
