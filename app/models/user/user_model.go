9ce150e85ab6f6e480631282ce05f4b0c2db42d0
zhangbaoqiang <1099457817@qq.com>
2023-02-12 23:33:08 +0800

    编辑个人资料

--- app/models/user/user_model.go
+++ app/models/user/user_model.go
@@ -9,10 +9,14 @@ import (
 // User用户模型
 type User struct {
 	models.BaseModel
-	Name     string `json:"name,omitempty"`
-	Email    string `json:"-"`
-	Phone    string `json:"-"`
-	Password string `json:"-"`
+	Name string `json:"name,omitempty"`
+
+	City         string `json:"city,omitempty"`
+	Introduction string `json:"introduction,omitempty"`
+	Avator       string `json:"avatar,omitempty"`
+	Email        string `json:"-"`
+	Phone        string `json:"-"`
+	Password     string `json:"-"`
 	models.CommonTimestampsField
 }
 
